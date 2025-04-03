2188d8decb14eb708bcad5443b7acc99b3d08dbc
Liga Daine <liga.daine@accenture.com>
2025-04-03 16:36:46 +0200

    add more examples to oop approach project

--- oop-approach/src/oop/OopApp.java
+++ oop-approach/src/oop/OopApp.java
@@ -5,6 +5,34 @@ public class OopApp {
     public static void main(String[] args) throws Exception {
         System.out.println("OOP Approach");
         instantiatingObjects();
+        arbitraryNumberOfArguments();
+        parametersVsArguments();
+    }
+
+    private static void parametersVsArguments() {
+        printSeparator("Parameters vs Arguments");
+        int firstArgument = 1;
+        String secondArgument = "mystring";
+        // the values passed to a method are arguments
+        testMethod(firstArgument, secondArgument);
+    }
+
+    private static void testMethod(int firstParameter, String secondParameter){
+        // firstParameter and secondParameter are parameters defined in method 
+        System.out.println("Parameter value: " + firstParameter);
+        System.out.println("Parameter value: " + secondParameter);
+    }
+    private static void arbitraryNumberOfArguments() {
+        printSeparator("Arbitrary number of arguments in methods");
+        //defining method when you dont know how many attributes should be provided
+        printStrings("11", "12");
+        printStrings("21", "22", "23", "24", "25");
+    }
+
+    private static void printStrings(String... values){
+        for(String value : values){
+            System.out.println(value);
+        }
     }
 
     private static void instantiatingObjects() {
